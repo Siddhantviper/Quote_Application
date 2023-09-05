0c28815a070457fc553f828c6695d384656a76dc
Siddhant Kumar Sahu <siddhantkumarsahu2022@gmail.com>
2023-09-05 16:09:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quoteappnew/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.quoteappnew
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+    lateinit var mainViewModel: MainViewModel
+    private val quoteText: TextView
+        get() = findViewById(R.id.quoteText)
+
+    private val quoteAuthor: TextView
+        get() = findViewById(R.id.quoteAuthor)
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mainViewModel = ViewModelProvider(this, MainViewModelFactory(application)).get(MainViewModel::class.java)
+        setQuote(mainViewModel.getQuote())
+    }
+
+    fun setQuote(quote:Quote){
+        quoteText.text = quote.text
+        quoteAuthor.text = quote.author
+    }
+
+    fun onPrevious(view: View) {
+        setQuote(mainViewModel.previousQuote())
+    }
+
+    fun onNext(view: View) {
+        setQuote(mainViewModel.nextQuote())
+    }
+    fun onFavorite(view: View) {
+       Toast.makeText(this,"I am Clicked",Toast.LENGTH_SHORT).show()
+    }
+
+    fun onShare(view: View) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.setType("text/plain")
+        intent.putExtra(Intent.EXTRA_TEXT, mainViewModel.getQuote().text)
+        startActivity(intent)
+    }
+
+}
